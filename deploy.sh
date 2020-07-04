0d9e03c5a57503911871d1ffa73be0aef6cd653a
korostelevm <korostelevm@gmail.com>
2020-07-04 18:09:51 -0400

    asdf

--- deploy.sh
+++ deploy.sh
@@ -26,8 +26,8 @@ set -x
 # npm run build:microfrontend
 
 sam package --template-file template.yaml --s3-bucket ${bucket} --s3-prefix ${app_name}-${version} --output-template-file template-built.yaml
-sam deploy --template-file template-built.yaml --stack-name ${app_name} --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset
+sam deploy --template-file template-built.yaml --stack-name ${app_name} --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides ServiceSubDomainName=freeze
 
 rm template-built.yaml
 cd vue && npm run build
-aws s3 cp ./dist s3://wtf.coldlambda.com --recursive  
+aws s3 cp ./dist s3://freeze.coldlambda.com --recursive  
